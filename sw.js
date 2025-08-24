9030af00c967659d0557a37caaa27e255c9a37b8
zintop <mthz3kg.ge@gmail.com>
2025-08-24 17:31:48 +0800

    Site updated: 2025-08-24 17:31:47

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1756018423325";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1756027761581";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
